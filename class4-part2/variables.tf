042f86b2c4c82e2326d26f16f15b9d5b894602cd
mackpat3 <mackpat3@gmail.com>
2025-07-31 00:06:18 +0000

    class 4part2 redo

--- class4-part2/variables.tf
+++ class4-part2/variables.tf
@@ -16,3 +16,40 @@ variable port {
 variable ec2_web {
   type = map(string)
 }
+
+variable vpc {
+  type = object({
+    cidr = string
+    name = string
+
+  })
+  default = {
+    cidr = ""
+    name = ""
+  }
+}
+
+variable subnet {
+  type = list(object({
+    cidr = string
+    az = string
+    name = string
+  }))
+  default = [
+    {
+    cidr = ""
+    az = ""
+    name = ""
+  },
+      {
+    cidr = ""
+    az = ""
+    name = ""
+  },
+      {
+    cidr = ""
+    az = ""
+    name = ""
+  }
+  ]
+}
